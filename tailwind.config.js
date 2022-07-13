910d87d0e6ba32922316abfe69cf7b0bc3f87e11
Christoffer Baadsgaard <christoffer@baadsgaard.net>
2022-07-13 17:50:44 +0200

    updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
   ],
 }
